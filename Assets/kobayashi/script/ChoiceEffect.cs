aad57b1e484047d235e3e678b4e97db7022ae7fb
kobayashi koutarou <147145211+kobakoGlhf@users.noreply.github.com>
2024-07-15 12:12:56 +0900

    エフェクトの表示など

--- Assets/kobayashi/script/ChoiceEffect.cs
+++ Assets/kobayashi/script/ChoiceEffect.cs
@@ -3,25 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ChoiceEfect : MonoBehaviour
+public class ChoiceEffect : MonoBehaviour
 {
     //[SerializeField] Choices _choices;
     [SerializeField] Image _panel;
-    [SerializeField] Text _text;
-    public void AnimationPlay(string choicesString)
+    [SerializeField] public Text _text;
+    private void Start()
     {
         _panel.gameObject.SetActive(true);
         _text.gameObject.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+    public void AnimationPlay(string choicesString)
+    {
+        this.gameObject.SetActive(true);
         _text.text = choicesString;
         _panel.GetComponent<Animator>().Play("PanelFadeIn");
         _text.GetComponent<Animator>().Play("TextFadeIn");
+        Invoke("AnimationReset1", 2f);
 
     }
-    public void AnimationReset()//アニメーションイベントで呼んでる
+    public void AnimationReset1()//アニメーションイベントで呼んでる
     {
         _panel.color = new Color(default, default, default, 0);
         _text.color= new Color(default, default, default, 0);
-        _panel.gameObject.SetActive(false);
-        _text.gameObject.SetActive(false);
+        this.gameObject.SetActive(false);
     }
 }
